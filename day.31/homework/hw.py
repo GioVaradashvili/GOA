e5121f9ae8b3f31788cf167b7fca72d28b2aa11e
GioVaradashvili <153834276+GioVaradashvili@users.noreply.github.com>
2024-08-29 12:09:21 -0700

    1

---
+++ day.31/homework/hw.py
@@ -0,0 +1,42 @@
+# 1
+
+# numbers = list(range(1, 11))
+# print(numbers)
+
+# 2
+
+# numbers = list(range(1,15))
+# print(numbers)
+# five_element = numbers[:5]
+# print("first five element's:", five_element)
+
+# 3
+
+# number = list(range(1.9)[-3:])
+# print(number)
+# minus3 = number[-3:]
+# print("ბოლო 3 ელემენტი:", minus3)
+
+# 4
+
+# numbers = list(range(1,2,3,4,5,6,7,8,9,10))
+# print(numbers)
+# every_second = numbers[:2]
+# print(every_second) 
+
+# 5
+
+# fruits = [ 'strowberry', 'apple', 'pinapple', 'alucha', 'jam']
+# print(fruits)
+
+# 6
+
+# fruits = list(range(1,2,3,4,5,6,7,8,9,10,11,12,13))
+# fruit= fruits[::-1]
+# print(fruit) 
+
+# 7
+
+fruit_list = ['kivi', 'soko', 'alucha', 'yurdzeni', 'vashli']
+fruit_list[2:4] = ['კივი', 'ცაცხვი']
+print(fruit_list) 
