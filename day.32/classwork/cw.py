a447a4f945504cd1cb49d52e8f2a49712e28e72e
GioVaradashvili <153834276+GioVaradashvili@users.noreply.github.com>
2024-09-07 11:03:58 -0700

    1

---
+++ day.32/classwork/cw.py
@@ -0,0 +1,44 @@
+# def count():
+#     for i in range(1,100,20):
+#         print(i)
+
+# count()
+# count()
+# count()
+# count()
+# count()
+
+
+
+
+# 1
+
+
+# def sum(x,y):
+#     print(x + y)
+#     print(x - y)
+#     print(x // y)
+#     print(x * y)
+# sum(44,24)
+
+# 2
+
+# text = input("enter text: ")
+# print(text)
+# input("gamarjoba gio ketili iyos sheni mobrzaneba gisurveb warmatebas da win svlas <3")
+
+# 3
+
+# leqsi_sikvarulze = "miyvarxar zlier zlier rac gadis dro da xani me ufro metad miyvars sheni patara tani sheni shavi tmis buchqi tetr shublze sheni titebi miyvars ori patara tati oo ici rogor miyvars odnav shexebac mati oo ici rogor miyvars gamoutqmeli enit sheni grznobebis sidable sulis simagle sheni me dardic miyvars sheni sheni kiskisic martobs me zlier me chrdili miyvars sheni ra aris fasi me chrdili miyvars sheni mzis dabnelebis msgavsi miyvarxar zlier zlier mwvavs sheni trfobisa ali miyvarxar zlier zlier mteli grznobit da zalit gamit chemtanxar sizmrad fiqrshi chemtan xar dgisit gulis zgeris xma mesmis sheni xma aris isic miyvarxar zlier zlier miyvarxar cxadad malvit rogorc arodens arvin ar hyvarebia arvis! es es sikvdils udris titqmis es sheudzloa titqmis shen erts gekutvnis yvela rac siyvarulad itqmis "
+# print(leqsi_sikvarulze)
+# input("siyvarulit giorgi varadashvili")
+
+# 4
+
+# name = "gio"
+# last_name = "varadashvili"
+# age = 15
+# hobies = "basketball"
+# country = "Georgia"
+
+# print(name, last_name, age, hobies, country)
