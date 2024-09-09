861739f5fb0bc4760ef2c0f0796888fca32324e7
GioVaradashvili <153834276+GioVaradashvili@users.noreply.github.com>
2024-09-09 10:13:14 -0700

    1

---
+++ day.35/homework/hw.py
@@ -0,0 +1,143 @@
+# 1
+
+# fruits = ["apple", "banana", "cherry", "date", "elderberry"]
+# print(fruits)
+
+# 2
+
+# fruits = ["apple", "carrot", "banana", "cherry", "berry"]
+# print("First item:", fruits[0])
+# print("Last item:", fruits[-1])
+
+# 3
+
+# fruits = ["apple", "banana", "berry"]
+# fruits.append("fig")
+# print(fruits)
+
+# 4
+
+# fruits = ["apple", "banana", "berry", "club"]
+# fruits.remove("banana")
+# print(fruits)
+
+# 5
+
+# fruits = ["apple", "berry", "banana"]
+# fruits[1] = "blueberry"
+# print(fruits)
+
+# 6
+
+# numbers = [10, 20, 30, 40, 50, 60, 70, 80, 90]
+# print(numbers)
+
+# 7
+
+# numbers = [10,20,30,40,50,60,70,80,90,]
+# numbers.append(100)
+# print(numbers)
+
+# 8
+
+# numbers = [0,1,2,3,4,5,6,7,8,9,]
+# numbers.insert(0, 5)
+# print(numbers)
+
+# 9
+
+# numbers = [10,20,30,40,50,60,70,80,90]
+# numbers.remove(30)
+# print(numbers)
+
+# 10
+
+# numbers = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15]
+# numbers.sort()
+# print(numbers)
+
+# 11
+
+# numbers = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20]
+# numbers.reverse()
+# print(numbers)
+
+# 12
+
+# numbers = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20]
+# index = numbers.index(50)
+# print("Index of 50:", index)
+
+# 13
+
+# numbers = [10,20,30,40,50,60,70,80,90,]
+# count_of_20 = numbers.count(20)
+# print("Count of 20:", count_of_20)
+
+# 13
+
+# numbers = [1]
+# numbers = list(range(1, 11))
+# print(numbers)
+
+# 14
+
+# numbers = [1,2,3,4,5,6,7,8,9,]
+# first_half = numbers[:5]
+# print(numbers)
+
+# 15
+
+# numbers = [1,2,3,4,5,6,7,8,9]
+# second_half = numbers[5:]
+# print(numbers)
+
+# 16
+
+# numbers = [1,2,3,4,5,6,7,8,9,10]
+# squares = [x**2 for x in numbers]
+# print(numbers)
+
+# 17
+
+# first_half = [1,2,3,4,5,6]
+# second_half = [1,2,3,4,5,6,7,8,]
+# squares = [1,2,3,4,5,6,7,8,9,10]
+
+# print("First half:", first_half)
+# print("Second half:", second_half)
+# print("Squares:", squares)
+
+# 18
+
+# temperatures = [72, 68, 75, 70, 78, 74, 71]
+# print(temperatures)
+
+# 19
+
+# temperatures = [10,20,30,40,50,60,70,80]
+# highest_temp = max(temperatures)
+# print("Highest temperature:", highest_temp)
+
+# 20
+
+# temperatures = [1,2,3,4,5,6,7,8,9]
+# lowest_temp = min(temperatures)
+# print("Lowest temperature:", lowest_temp)
+
+# 21
+
+# temperatures = [1,2,3,4,5,6,7,8,9,10]
+# average_temp = sum(temperatures) / len(temperatures)
+# print("Average temperature:", average_temp)
+
+# 22
+
+# temperatures = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20]
+# above_70 = [temp for temp in temperatures if temp > 70]
+# print(temperatures)
+
+# 23
+
+# above_70 =[10,20,30,40,50,60,70,80,90,100]
+# print("Temperatures above 70:", above_70)
