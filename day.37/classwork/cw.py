7eb64138273fe9d6f473359f3ac6b239037dc907
GioVaradashvili <153834276+GioVaradashvili@users.noreply.github.com>
2024-09-10 11:44:45 -0700

    1

---
+++ day.37/classwork/cw.py
@@ -0,0 +1,39 @@
+# 1
+
+# name = input(format('your name is:'))
+# age = input(format('your age is:'))
+
+# print(format('giorgi'))
+# print(format('15'))
+
+# input('hello giorgi')
+# input('your age is 15')
+
+
+# 2
+
+# text_list = ['python', 'is', 'fun']
+# print(text_list)
+
+# 3
+
+# info = input(format('giorgi varadashvili'))
+# print(info)
+
+# 4
+
+# name =input(format('your name is:'))
+
+# if name == 'giorgi':
+#     print('Hello Admin!')
+
+# 5
+
+# name = 'GIORGI VARADASHVILI'
+
+# print(name.lower())
+
+# 6
+
+# name = 'giorgi varadashvili'
+# print(name.upper())
