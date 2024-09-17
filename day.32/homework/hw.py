eec788680638882c4236623701d6a14349004ea0
GioVaradashvili <153834276+GioVaradashvili@users.noreply.github.com>
2024-09-17 04:53:45 -0700

    1

---
+++ day.32/homework/hw.py
@@ -0,0 +1,43 @@
+# 1
+
+# numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+# print(numbers)
+
+# 2
+
+# numbers = [1, 2, 3, 4, 5, 6,]
+# first_five = numbers[:5]
+# print(first_five)
+
+
+# 3
+
+
+# numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+# last_three = numbers[-3:]
+# print(last_three)
+
+# 4
+
+# numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+# every_second = numbers[::2]
+# print(every_second)
+
+
+# 5
+
+# fruits = ['ვაშლი', 'ბანანი', 'ალუბალი', 'თარიღი', 'ლეღვი', 'ყურძენი']
+# print(fruits)
+
+# 6
+
+# fruits = ['ვაშლი', 'ბანანი', 'ალუბალი', 'თარიღი', 'ლეღვი', 'ყურძენი']
+# reversed_fruits = fruits[::-1]
+# print(reversed_fruits)
+
+# 7
+
+# fruits = ['ვაშლი', 'ბანანი', 'ალუბალი', 'თარიღი', 'ლეღვი', 'ყურძენი']
+# fruits[2:4] = ['კივი', 'ტყემალი']
+# print(fruits)
+
