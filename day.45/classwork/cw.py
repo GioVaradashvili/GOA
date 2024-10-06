5692e6e4d4fa1352e15978e7f6284ab2d57454f5
GioVaradashvili <153834276+GioVaradashvili@users.noreply.github.com>
2024-10-06 12:32:57 -0700

    1

---
+++ day.45/classwork/cw.py
@@ -0,0 +1,23 @@
+
+
+# 1
+# def printBill(costumer_name):
+#     print("Bill")
+#     print(costumer_name)
+#     print("Bob")
+
+# 2
+
+# def welcome(name):
+#     print("Hello", name)
+#     print("Welcome user")
+
+
+# 3
+
+# def area(x,y):
+#     return x,y
+# w = int(input())
+# h = int(input())
+
+# print(area(w, h))
