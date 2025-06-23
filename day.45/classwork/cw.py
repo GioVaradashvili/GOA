750180fd6d8066560f07b6cdb761fc8aa177763c
GioVaradashvili <153834276+GioVaradashvili@users.noreply.github.com>
2025-06-23 10:48:37 -0700

    1

--- day.45/classwork/cw.py
+++ day.45/classwork/cw.py
@@ -1,23 +1,29 @@
+# 2. ფუნქცია: რაოდენობის დათვლა
+def count_items(item_list, item):
+    count = 0
+    for i in item_list:
+        if i == item:
+            count += 1
+    return count
 
+# 3. ფუნქცია: ჯამის გამოთვლა
+def sum_of_list(num_list):
+    total = 0
+    for num in num_list:
+        total += num
+    return total
 
-# 1
-# def printBill(costumer_name):
-#     print("Bill")
-#     print(costumer_name)
-#     print("Bob")
+# 4. ფუნქცია: საშუალო მნიშვნელობის გამოთვლა
+def average_of_list(num_list):
+    total = sum_of_list(num_list)
+    count = len(num_list)
+    if count == 0:  # სია არ უნდა იყოს ცარიელი
+        return 0
+    return total / count
 
-# 2
-
-# def welcome(name):
-#     print("Hello", name)
-#     print("Welcome user")
-
-
-# 3
-
-# def area(x,y):
-#     return x,y
-# w = int(input())
-# h = int(input())
-
-# print(area(w, h))
+# 5. ფუნქცია: სიის გადაბრუნება
+def reverse_list(items):
+    reversed_list = []
+    for item in items:
+        reversed_list.insert(0, item)
+    return reversed_list
---
+++ day.46/homework/hw.py
@@ -0,0 +1,1 @@
+# yvela sololerni mzadaa
